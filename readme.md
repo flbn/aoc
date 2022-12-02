a83e3c2844107918662f272bdaacb9b3d5852554
flbn <overasc@gmail.com>
2022-12-01 22:28:54 -0500

    init clap stuff

---
+++ readme.md
@@ -0,0 +1,19 @@
+### aoc
+
+### cli args
+```bash
+Usage: aoc [OPTIONS]
+
+Options:
+-y, --year <YEAR>
+Year of exercise [default: 2022]
+-d, --day <DAY>
+Day of the month [default: 1] [default: 1]
+-h, --help
+Print help information
+-V, --version
+Print version information
+```
+
+### run
+`cargo run -- -y 2022 -d 1`
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[path = "2022/mod.rs"]
+mod twenty_twenty_two;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(next_line_help = true)]
+struct Args {
+    /// Year of exercise
+    #[arg(short = 'y', long = "year", default_value_t = 2022)]
+    year: u16,
+
+    /// Day of the month [default: 1]
+    #[arg(short = 'd', long = "day", default_value_t = 1)]
+    day: u8,
+}
+
+fn main() {
+    let cli = Args::parse();
+    match cli.year {
+        2022 => match cli.day {
+            1 => twenty_twenty_two::one::main(),
+            _ => print!("no such day!"),
+        },
+        _ => print!("no such year!"),
+    }
+}
